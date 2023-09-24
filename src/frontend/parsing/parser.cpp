LincolnSand/foo_cc

commit 4272a9eeca901b5da695bfdfc79e1857c14fe335

Fix typedef and struct declaration parsing
Signed-off-by: LincolnSand <sandlincoln@gmail.com>

src/frontend/parsing/parser.cpp
@@ -782,6 +782,21 @@ void add_unsigned_integer_types_to_type_table(parser_t& parser) {
     unsigned_integer_symbol_table.insert({"unsigned long long", make_primitive_type(token_type_t::UNSIGNED_LONG_LONG_KEYWORD)});
 }
 
+bool is_potential_type(parser_t& parser) {
+    if(is_struct_keyword(parser.peek_token())) {
+        if(parser.peek_token_n(1).token_type == token_type_t::IDENTIFIER) {
+            return true;
+        }
+        return false;
+    }
+    if(is_keyword_a_type(parser.peek_token().token_type)) {
+        return true;
+    }
+    if(parser.peek_token().token_type == token_type_t::IDENTIFIER) {
+        return true;
+    }
+    return false;
+}
 bool is_a_type(const parser_t& parser) {
     if(is_struct_keyword(parser.peek_token())) {
         const auto identifier_token = parser.peek_token_n(1);
@@ -1049,7 +1064,6 @@ ast::type_t parse_typedef_struct_decl_or_def(parser_t& parser) {
             throw std::runtime_error("Struct [" + name + "] already defined.");
         }
         else {
-            parser.advance_token();
             // parse struct declaration
             auto struct_forward_decl = make_struct_forward_decl_type_t(name);
             if(!utils::contains(struct_type_table, name)) {
@@ -1057,7 +1071,7 @@ ast::type_t parse_typedef_struct_decl_or_def(parser_t& parser) {
             }
             return struct_forward_decl;
         }
-    } else if(name_token.token_type == token_type_t::IDENTIFIER) {
+    } else if(name_token.token_type == token_type_t::LEFT_CURLY) {
         return parse_and_validate_anonymous_typedef_struct_definition(parser);
     } else {
         throw std::runtime_error("Expected identifier name in struct declaration/definition.");
@@ -1176,7 +1190,7 @@ ast::compound_statement_t parse_and_validate_compound_statement(parser_t& parser
             throw std::runtime_error("Unexpected end of file. Unterminated compound statement.");
         }
 
-        if(is_a_type(parser)) {
+        if(is_potential_type(parser)) {
             ret.stmts.push_back(parse_and_validate_declaration(parser));
         } else {
             ret.stmts.push_back(parse_and_validate_statement(parser));

src/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
 
             //type_check(ast);
 
-            //print_validated_ast(ast);
+            print_validated_ast(ast);
         //} catch(const std::runtime_error &e) {
             // Temporary while we are fuzzing.
             // TODO: Implement proper error handling for compile errors.

test_programs/main13.c
@@ -3,12 +3,12 @@ struct foo {
     float t;
 };
 
-//typedef struct foo foo_t;
+typedef struct foo foo_t;
 
 int main() {
-    //foo_t f;
-    //struct foo f;
-    struct foo_t f;
+    foo_t f1;
+    struct foo f2;
+    //struct foo_t f;
 
     int v = 7;
     if(v) {

commit aa6a28ef16f6a50b34f37a3743b0475ec031dc37

Fix bug introduced in 4272a9eeca901b5da695bfdfc79e1857c14fe335
I had introduced a function called `is_potential_type`, but it shouldn't have been created and totally broke expression statements starting with variables.

Signed-off-by: LincolnSand <sandlincoln@gmail.com>

src/frontend/parsing/parser.cpp
@@ -782,21 +782,6 @@ void add_unsigned_integer_types_to_type_table(parser_t& parser) {
     unsigned_integer_symbol_table.insert({"unsigned long long", make_primitive_type(token_type_t::UNSIGNED_LONG_LONG_KEYWORD)});
 }
 
-bool is_potential_type(parser_t& parser) {
-    if(is_struct_keyword(parser.peek_token())) {
-        if(parser.peek_token_n(1).token_type == token_type_t::IDENTIFIER) {
-            return true;
-        }
-        return false;
-    }
-    if(is_keyword_a_type(parser.peek_token().token_type)) {
-        return true;
-    }
-    if(parser.peek_token().token_type == token_type_t::IDENTIFIER) {
-        return true;
-    }
-    return false;
-}
 bool is_a_type(const parser_t& parser) {
     if(is_struct_keyword(parser.peek_token())) {
         const auto identifier_token = parser.peek_token_n(1);
@@ -1215,7 +1200,7 @@ ast::compound_statement_t parse_and_validate_compound_statement(parser_t& parser
             throw std::runtime_error("Unexpected end of file. Unterminated compound statement.");
         }
 
-        if(is_potential_type(parser)) {
+        if(is_a_type(parser)) {
             ret.stmts.push_back(parse_and_validate_declaration(parser));
         } else {
             ret.stmts.push_back(parse_and_validate_statement(parser));

src/main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char** argv) {
             parser_t parser(tokens_list);
             ast::validated_program_t ast = parse(parser);
 
-            //type_check(ast);
+            type_check(ast); // mutates `valid_ast`
 
             print_validated_ast(ast);
         //} catch(const std::runtime_error &e) {
@@ -51,8 +51,6 @@ int main(int argc, char** argv) {
             // TODO: Implement proper error handling for compile errors.
             //return 0;
         //}
-
-        //type_check(ast); // mutates `valid_ast`
 
         //std::string assembly_output = generate_asm(ast);
 

test_programs/main14.c
@@ -12,6 +12,8 @@ int main() {
     struct foo f3;
     //struct foo_t f;
 
+    f2;
+
     int v = 7;
     if(v) {
         double ll = 0.7f;