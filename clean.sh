cc5e897ca52783720633aa3f43600fd21b3cd7e9
LincolnSand <sandlincoln@gmail.com>
2023-10-29 01:49:36 -0600

    Fix clean.sh so that it no longer deletes GoogleTest or other subprojects/ files

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 rm -rf build
 rm -f log.txt
-find . -type f ! -name "*.*" -not -path "./.git/*" -not -path "./.vscode/*" ! -name "LICENSE" | xargs rm
+find . -type f ! -name "*.*" -not -path "./.git/*" -not -path "./.vscode/*" -not -path "./subprojects/*" ! -name "LICENSE" | xargs rm
 find . -type f -name "*.s" | xargs rm
